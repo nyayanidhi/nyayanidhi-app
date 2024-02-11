970d7d95fa196298bb051c6af94f63931c75a736
Adithya Krishna <adi@documenso.com>
2024-02-11 17:11:35 +0530

    chore: moved to app dir
    Signed-off-by: Adithya Krishna <adi@documenso.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from "next/font/google";
 import "./globals.css";
-import cn  from "clsx"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
